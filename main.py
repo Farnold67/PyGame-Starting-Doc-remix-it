9ee43276226c8392377a0c4e545a43414675ea67
Arnold Zou <52708162-Farnold@users.noreply.replit.com>
2026-01-07 22:37:39 +0000

    mom 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame.locals import QUIT
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption("hi")
+pygame.display.set_caption("weouhwiuerh")
 while True:
    for event in pygame.event.get():
        if event.type == QUIT:
